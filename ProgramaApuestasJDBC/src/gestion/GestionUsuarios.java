972fab0b3661b8031c6be00fc8f24736b38845a8
Angela <angyvazquezdel97>
2019-11-09 20:43:56 +0100

    Metodo gestion de usuarios
    rellena un objeto usuario con todos sus datos

--- ProgramaApuestasJDBC/src/gestion/GestionUsuarios.java
+++ ProgramaApuestasJDBC/src/gestion/GestionUsuarios.java
@@ -1,4 +1,61 @@
 package gestion;
 
+import clases.UsuarioImpl;
+import conexion.ConexionJDBC;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class GestionUsuarios {
+
+    /*
+     * Signatura: public UsuarioImpl obtenerObjetoUsuarioCompleto(UsuarioImpl usuario)
+     * Comentario: Construye el objeto usuario cogiendo los datos de la BBDD
+     * Precondiciones: El objeto usuario recibido debe tener una combinacion de correo y contraseña que debe existir en la BBDD
+     * Entradas:
+     * Salidas:
+     * Entrada/Salida: objeto usuario que primero tendra solo correo y contraseña y luego
+     *                  sera un objeto con los datos correspondientes a partir de la BBDD
+     * Postcondiciones: Modifica el objeto usuario añadiendo todos los datos que tuviera. Devuelve asociado al nombre
+     *                  un boolean que sera true si el usuario se construyo correctamente y false si hubo algun problema.
+     * */
+    public boolean obtenerObjetoUsuarioCompleto(UsuarioImpl usuario){
+        ConexionJDBC conexionJDBC = new ConexionJDBC();
+        Connection connection = conexionJDBC.getConnection();
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        boolean exito = false;
+
+        String miSelect = "select * from Usuarios  where correo = ?\n" +
+                "        and\n" +
+                "                contraseña = ?";
+        try {
+            //Preparo el statement
+            preparedStatement = connection.prepareStatement(miSelect);
+            preparedStatement.setString(1, usuario.getCorreo());
+            preparedStatement.setString(2, usuario.getPassword());
+            //Ejecuto
+            resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                //Le pongo los datos al objeto usuario
+                usuario.setId(resultSet.getInt("id"));
+                usuario.setCantidadActualDinero(resultSet.getDouble("saldo"));
+                usuario.setCorreo(resultSet.getString("correo"));
+                usuario.setPassword(resultSet.getString("contraseña"));
+                usuario.setAdmin(resultSet.getBoolean("isAdmin"));
+            }
+            exito = true;
+
+            //Cierro
+            resultSet.close();
+            preparedStatement.close();
+            conexionJDBC.closeConnection(connection);
+        }catch (SQLException e){
+            e.getStackTrace();
+        }
+        return exito;
+    }
 }
