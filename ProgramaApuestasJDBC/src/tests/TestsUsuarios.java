a94756cc73a97ddd0e4f8e37b87e321fc7a9ece2
Angela <angyvazquezdel97@gmail.com>
2019-11-11 10:56:07 +0100

    test

--- ProgramaApuestasJDBC/src/tests/TestsUsuarios.java
+++ ProgramaApuestasJDBC/src/tests/TestsUsuarios.java
@@ -1,7 +1,31 @@
 package tests;
 
+import clases.UsuarioImpl;
+import gestion.GestionUsuarios;
+
 public class TestsUsuarios {
     public static void main(String[] args) {
+        GestionUsuarios gestionUsuarios = new GestionUsuarios();
+        UsuarioImpl usuario = new UsuarioImpl();
+        usuario.setCorreo("angela@gmail.com");
+        usuario.setPassword("123");
 
+
+        System.out.println("Antes de obtener los datos");
+        System.out.println("Correo usuario: " + usuario.getCorreo());
+        System.out.println("Contraseña usuario: " + usuario.getPassword());
+        System.out.println("ID usuario: " + usuario.getId());
+        System.out.println("Saldo usuario: " + usuario.getCantidadActualDinero());
+        System.out.println("isAdmin: " + usuario.isAdmin());
+
+        gestionUsuarios.obtenerObjetoUsuarioCompleto(usuario);
+        System.out.println("Exito al obtener usuario completo: " + gestionUsuarios.obtenerObjetoUsuarioCompleto(usuario) );
+
+        System.out.println("Antes de obtener los datos");
+        System.out.println("Correo usuario: " + usuario.getCorreo());
+        System.out.println("Contraseña usuario: " + usuario.getPassword());
+        System.out.println("ID usuario: " + usuario.getId());
+        System.out.println("Saldo usuario: " + usuario.getCantidadActualDinero());
+        System.out.println("isAdmin: " + usuario.isAdmin());
     }
 }
