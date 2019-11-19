1e13c7a028a31a7a64cfd72b6fdb916a16b90dba
Angela <angyvazquezdel97@gmail.com>
2019-11-19 10:50:15 +0100

    Añadido nuevo atributo de la BBDD (isPeriodoApuestasAbierto)
    faltaria meter los get/set en la interface

--- ProgramaApuestasJDBC/src/clases/PartidoImpl.java
+++ ProgramaApuestasJDBC/src/clases/PartidoImpl.java
@@ -29,8 +29,9 @@ public class PartidoImpl implements Partido {
     private GregorianCalendar fechaFin;
     private String nombreLocal;
     private String nombreVisitante;
+    private boolean isPeriodoApuestasAbierto;
 
-    public PartidoImpl(int id, int golesLocal, int golesVisitante, GregorianCalendar fechaInicio, GregorianCalendar fechaFin, String nombreLocal, String nombreVisitante) {
+    public PartidoImpl(int id, int golesLocal, int golesVisitante, GregorianCalendar fechaInicio, GregorianCalendar fechaFin, String nombreLocal, String nombreVisitante, boolean isPeriodoApuestasAbierto) {
         this.id = id;
         this.golesLocal = golesLocal;
         this.golesVisitante = golesVisitante;
@@ -38,6 +39,7 @@ public class PartidoImpl implements Partido {
         this.fechaFin = fechaFin;
         this.nombreLocal = nombreLocal;
         this.nombreVisitante = nombreVisitante;
+        this.isPeriodoApuestasAbierto = isPeriodoApuestasAbierto;
     }
 
     public PartidoImpl(){
@@ -48,6 +50,7 @@ public class PartidoImpl implements Partido {
         this.fechaFin = new GregorianCalendar(1900,1,1);
         this.nombreLocal = " ";
         this.nombreVisitante = " ";
+        this.isPeriodoApuestasAbierto = false;
     }
     //Getters y setters
     public int getId() {
@@ -104,5 +107,13 @@ public class PartidoImpl implements Partido {
 
     public void setNombreVisitante(String nombreVisitante) {
         this.nombreVisitante = nombreVisitante;
+    }
+
+    public boolean isPeriodoApuestasAbierto() {
+        return isPeriodoApuestasAbierto;
+    }
+
+    public void setPeriodoApuestasAbierto(boolean periodoApuestasAbierto) {
+        isPeriodoApuestasAbierto = periodoApuestasAbierto;
     }
 }
