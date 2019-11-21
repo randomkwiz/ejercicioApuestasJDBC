e7c12e8a6b1765719a1f5c695cc0dc2cda72af8f
nzhdeh21 <nzhdeh1991@gmail.com>
2019-11-21 21:11:31 +0100

    terminar

--- ejercicioApuestasJDBCNzhdehNoTocar/ProgramaApuestasJDBC/src/gestion/GestionSaldo.java
+++ ejercicioApuestasJDBCNzhdehNoTocar/ProgramaApuestasJDBC/src/gestion/GestionSaldo.java
@@ -103,4 +103,43 @@ public class GestionSaldo {
 		}
 		return listadoIngresos;
 	}
+	
+	/*
+     * Signatura: public boolean realizarIngresoEnCuentaUsuario(UsuarioImpl usuario, IngresoImpl ingreso)
+     * Comentario: Realiza un ingreso de dinero en la cuenta del usuario.
+     * Precondiciones: El objeto usuario recibido debe existir en la BBDD
+     * Entradas: objeto usuario y objeto ingreso que sera el ingreso a introducir en la cuenta del usuario
+     * Salidas: boolean que indicara el exito de la operacion
+     * Entrada/Salida:
+     * Postcondiciones: Modifica el objeto usuario actualizando su saldo actual. Devuelve asociado al nombre
+     *                  un boolean que sera true si la operacion se realizo correctamente y false si hubo algun problema.
+     *                  La base de datos quedará actualizada con un ingreso económico.
+     * */
+    public boolean RealizarRetiradaEnCuentaUsuario(UsuarioImpl usuario, IngresoImpl retirada){
+        ConexionJDBC conexionJDBC = new ConexionJDBC();
+        Connection connection = conexionJDBC.getConnection();
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        boolean exito = false;
+
+        String miSelect = "update Ingresos set cantidad = \n" +
+                "VALUES(?,?,?)";
+        try {
+            //Preparo el statement
+            preparedStatement = connection.prepareStatement(miSelect);
+            preparedStatement.setDouble(1,retirada.getCantidad());
+            preparedStatement.setString(2, retirada.getDescripcion());
+            preparedStatement.setInt(3, usuario.getId());
+            //Ejecuto
+            preparedStatement.executeQuery();
+            exito = true;
+
+            //Cierro
+            preparedStatement.close();
+            conexionJDBC.closeConnection(connection);
+        }catch (SQLException e){
+            e.getStackTrace();
+        }
+        return exito;
+    }
 }
